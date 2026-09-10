8be1a85ad494c24dfe518418af420f0fcfa3d82b
mojirt37 <mojirt37@atomicmail.io>
2026-09-10 13:55:32 +0330

    feat: update 404.php with error message and search form

--- wp-content/themes/bambroo/404.php
+++ wp-content/themes/bambroo/404.php
@@ -8,17 +8,17 @@ get_header();
 ?>
 
 <main id="main-content" class="rtl">
-    <div class="error-404">
+    <div class="error-404 container text-center">
         <h1>۴۰۴</h1>
         <h2>صفحه یافت نشد</h2>
         <p>متأسفانه صفحه‌ای که به دنبال آن هستید، یافت نشد.</p>
         
-        <div class="error-actions">
+        <div class="error-actions margin-bottom">
             <a href="<?php echo esc_url(home_url('/')); ?>" class="button">بازگشت به صفحه اصلی</a>
             <a href="<?php echo esc_url(wc_get_page_permalink('shop')); ?>" class="button">بازگشت به فروشگاه</a>
         </div>
 
-        <div class="error-search">
+        <div class="error-search margin-bottom">
             <h3>جستجو در سایت</h3>
             <?php get_search_form(); ?>
         </div>
